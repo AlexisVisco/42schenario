0a36bc7c7a87818f94b743731d8c88ad6240698b
AlexisVisco <Alexis.viscogliosi@outlook.fr>
2018-06-04 17:20:16 +0200

    push lot of scenario

--- src/fr/aviscogl/Schenario.java
+++ src/fr/aviscogl/Schenario.java
@@ -7,18 +7,22 @@ import java.util.Arrays;
 
 public class Schenario {
     public static void main(String[] args) {
-        if (args.length != 1) {
+        System.out.println(Arrays.toString(args));
+        if (args.length == 0) {
             System.err.println("42Schenario > Need a text scenario.");
-            return;
-        }
-        else if (!new File(args[0]).exists()) {
-            System.err.printf("42Schenario > File %s does not exist.\n", args[0]);
             return;
         }
         System.out.println("42Schenario > Start in 5 seconds");
         try {
             Thread.sleep(5 * 1000);
-            Scenario.execute(args[0]);
+            for (String arg : args) {
+                if (!new File(arg).exists()) {
+                    System.err.printf("42Schenario > file %s does not exist\n", arg);
+                    continue;
+                }
+                System.out.printf("\n42Schenario > Executing %s !", arg);
+                Scenario.execute(arg);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
